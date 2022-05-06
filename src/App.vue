d14b49499c341c55debb53969fe5bfcb832bbc3e
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-05-06 05:35:49 +0200

    HTTP Post request

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <h1>vue-http</h1>
+  <create-post />
   <post-list />
 </template>
 
 <script>
+import CreatePost from "./components/CreatePost.vue";
 import PostList from "./components/PostList.vue";
 export default {
   name: "App",
-  components: { PostList },
+  components: { PostList, CreatePost },
 };
 </script>
 
---
+++ src/components/CreatePost.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="createPost">
+    <label for="title">title</label>
+    <input id="title" type="text" v-model="formData.title" />
+    <label for="body">body</label>
+    <input id="body" type="text" v-model="formData.body" />
+    <label for="userId">userId</label>
+    <input id="userId" type="text" v-model="formData.userId" />
+    <button>Create Post</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      formData: {
+        title: "",
+        body: "",
+        userId: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", this.formData)
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
